585d57067685ff0932bd2e50d2cd0b4a4b0a7289
sanjai <sanjai9608@gmail.com>
2025-09-03 13:32:35 +0530

    remove unused commands

--- src/controllers/roomController.ts
+++ src/controllers/roomController.ts
@@ -12,14 +12,11 @@ export async function getRooms(req: Request, res: Response) {
             is_available: true
         })
         if (!getRooms || getRooms.length === 0) {
-            // return res.status(404).json({ message: "No available rooms found" });
             return sendErrorResponse(res,MESSAGES.ROOMS_NOT_AVAILABLE)
         }
-        // return res.status(200).json(getRooms);
         return successResponse(res,getRooms,MESSAGES.DATA_SUCCESS)
     } catch (error) {
         console.error('Error fetching rooms:', error);
-        // return res.status(500).json({ message: "Internal server error", error: error });
         return sendErrorResponse(res,MESSAGES.SERVER_FETCH_ERROR)
     }
 }
@@ -29,7 +26,6 @@ export async function createRooms(req: Request, res: Response) {
         const { room_number, room_type, is_available } = req.body;
         const price = getRoomDetails(room_type)
         if (price === 0) {
-            // return res.status(400).json({ message: "Invalid room type provided" });
             return sendErrorResponse(res,MESSAGES.ROOMS_NOT_AVAILABLE)
         }
         
@@ -40,14 +36,9 @@ export async function createRooms(req: Request, res: Response) {
         create_room.is_available = is_available;
 
         const savedRoom = await roomRepository.save(create_room)
-        // return res.status(201).json({
-        //     message: "Room created successfully",
-        //     hotel: savedRoom
-        // });
         return successResponse(res,savedRoom,MESSAGES.DATA_SUCCESS)
     } catch (error) {
         console.error('Error fetching rooms:', error);
-        // return res.status(500).json({ message: "Internal server error", error: error });
         return sendErrorResponse(res,MESSAGES.SERVER_FETCH_ERROR)
     }
 }
